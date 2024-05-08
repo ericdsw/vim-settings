3a07b9a073e8d5fb398e2656dd5aca069251eed3
Eric De Sedas <eric.desedas.w@gmail.com>
2024-05-08 09:51:26 -0500

    Tweaks to luasnip and treesitter

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,14 +1,14 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
+    version = "v2.*", 
     build = "make install_jsregexp",
     keys = function()
       return {}
     end,
     config = function()
-      require("luasnip.loaders.from_snipmate").lazy_load()
+      -- require("luasnip.loaders.from_snipmate").load()
+      require("luasnip.loaders.from_snipmate").lazy_load({paths = "~/.config/nvim/snippets"})
     end
   }
 }
