2ec824a9e74b68e5604bbec628f193d528737812
Eric <eric.desedas.w@gmail.com>
2024-05-07 20:15:09 -0500

    Added vim-surround, updated telescope configuration

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,6 +10,51 @@ return {
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
       vim.keymap.set('n', '<C-p>', builtin.find_files, { silent = true })
+
+      local previewers = require("telescope.previewers")
+      local Job = require("plenary.job")
+      local new_maker = function(filepath, bufnr, opts)
+        filepath = vim.fn.expand(filepath)
+        Job:new({
+          command = "file",
+          args = { "--mime-type", "-b", filepath },
+          on_exit = function(j)
+            local mime_type = vim.split(j:result()[1], "/")[1]
+            if mime_type == "text" then
+              previewers.buffer_previewer_maker(filepath, bufnr, opts)
+            else
+              -- maybe we want to write something to the buffer here
+              vim.schedule(function()
+                vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY" })
+              end)
+            end
+          end
+        }):sync()
+      end
+
+      local actions = require("telescope.actions")
+      require("telescope").setup{
+        defaults = {
+          mappings = {
+            i = {
+              ["<esc>"] = actions.close
+            }
+          },
+          preview = {
+            filesize_limit = 0.3
+          },
+          buffer_previewer_maker = new_maker,
+          file_ignore_patterns = {
+            "node_modules",
+            "%.png",
+            "%.jpg",
+            "%.webp",
+            "%.wav",
+            "%.ogg",
+            "%.ico"
+          }
+        }
+      }
     end
   }
 }
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })()
+    end
+  }
+}
