c052cc47e75b555d5dd4002bff353907b09d0a3f
Eric De Sedas <eric.desedas.w@gmail.com>
2026-02-12 09:45:47 -0500

    updated colorscheme

--- nvim/lua/eric/plugins/colorschemes.lua
+++ nvim/lua/eric/plugins/colorschemes.lua
@@ -1,14 +1,25 @@
 return {
   {
-    'sainnhe/everforest',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.everforest_enable_italic = true
-      vim.o.background = "dark"
-      vim.cmd.colorscheme('everforest')
-    end
-  },
+      'sainnhe/gruvbox-material',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.gruvbox_material_enable_italic = true
+        vim.cmd.colorscheme('gruvbox-material')
+      end
+    }
+  -- {
+  --   'sainnhe/everforest',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.everforest_enable_italic = true
+  --     vim.o.background = "dark"
+  --     vim.cmd.colorscheme('everforest')
+  --   end
+  -- },
   -- {
   --   'AlexvZyl/nordic.nvim',
   --   lazy = false,
