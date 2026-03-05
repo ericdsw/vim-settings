f2f37cd58d64cc09613f0218914ac6edc6cea930
Eric De Sedas <eric.desedas.w@gmail.com>
2026-03-04 20:49:09 -0500

    More colorscheme updates

--- nvim/lua/eric/plugins/colorschemes.lua
+++ nvim/lua/eric/plugins/colorschemes.lua
@@ -1,5 +1,29 @@
 return {
   -- {
+  --   'folke/tokyonight.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme('tokyonight-moon')
+  --   end
+  -- } 
+  {
+    'rmehri01/onenord.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('onenord').setup()
+    end
+  }
+  -- {
+  --   'jnurmine/Zenburn',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme('zenburn')
+  --   end
+  -- }
+  -- {
   --   'AlexvZyl/nordic.nvim',
   --   lazy = false,
   --   priority = 1000,
@@ -25,32 +49,24 @@ return {
   --   end
   -- }
   -- {
-  --     'sainnhe/gruvbox-material',
-  --     lazy = false,
-  --     priority = 1000,
-  --     config = function()
-  --       -- Optionally configure and load the colorscheme
-  --       -- directly inside the plugin declaration.
-  --       vim.g.gruvbox_material_enable_italic = true
-  --       vim.cmd.colorscheme('gruvbox-material')
-  --     end
-  --   }
-  {
-    'sainnhe/everforest',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.everforest_enable_italic = true
-      vim.o.background = "dark"
-      vim.cmd.colorscheme('everforest')
-    end
-  },
-  -- {
-  --   'AlexvZyl/nordic.nvim',
+  --   'sainnhe/gruvbox-material',
   --   lazy = false,
   --   priority = 1000,
   --   config = function()
-  --       require('nordic').load()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.gruvbox_material_enable_italic = true
+  --     vim.cmd.colorscheme('gruvbox-material')
+  --   end
+  -- }
+  -- {
+  --   'sainnhe/everforest',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.everforest_enable_italic = true
+  --     vim.o.background = "dark"
+  --     vim.cmd.colorscheme('everforest')
   --   end
   -- },
 }
