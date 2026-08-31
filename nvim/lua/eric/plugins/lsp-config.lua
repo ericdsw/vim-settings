f30bb8c35fc7904001b98afd0f758d8e95db4aab
Eric De Sedas <eric.desedas.w@gmail.com>
2026-08-30 19:05:07 -0500

    Tweaks to rust lspconfig

--- nvim/lua/eric/plugins/lsp-config.lua
+++ nvim/lua/eric/plugins/lsp-config.lua
@@ -17,8 +17,46 @@ return {
       require('mason').setup()
       local mason_lspconfig = require 'mason-lspconfig'
       mason_lspconfig.setup {
-        ensure_installed = { "lua_ls", "ts_ls", "pyright" }
+        ensure_installed = { "lua_ls", "ts_ls", "pyright", "rust_analyzer" }
       }
+
+      -- Rust
+      vim.lsp.config("rust_analyzer", {
+        settings = {
+          ["rust_analyzer"] = {
+            imports = {
+              granularity = {
+                group = "module",
+              },
+              prefix = "self",
+            }
+          },
+          cargo = {
+            buildScripts = {
+              enable = true,
+            },
+          },
+          procMacro = {
+            enable = true
+          },
+        },
+      })
+      vim.lsp.enable("rust_analyzer")
+      -- Enable different LSP features.
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(ev)
+          local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+          -- Inlay hints display inferred types, etc.
+          if client:supports_method("inlayHint/resolve") then
+            vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+          end
+          -- Completion can be invoked via ctrl+x ctrl+o. It displays a luist of
+          -- names inferred from the context (e.g. method names, variables, etc.)
+          if client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, ev.buf, {})
+          end
+        end,
+      })
 
       -- Lua
       vim.lsp.config('lua_ls', {
